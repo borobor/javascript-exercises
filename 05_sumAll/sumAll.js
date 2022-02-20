73714b5f18156cc56ecdea5a432b9c85fa2d189f
borobor <bornajurisa@gmail.com>
2022-02-20 13:05:58 +0100

    Write a function that passes first 2 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, lastNum) {
+	let sum = 0;
+	for (let i = firstNum; i <= lastNum; i++) {
+		sum += i;
+	}
+	return sum;
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
