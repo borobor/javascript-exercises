7d6c8044516aca1744cd573f3ff9c7a00f7c4612
borobor <bornajurisa@gmail.com>
2022-02-20 13:28:00 +0100

    Modify function to pass 3 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,21 @@
 const sumAll = function(firstNum, lastNum) {
 	let sum = 0;
-	for (let i = firstNum; i <= lastNum; i++) {
+	let startNum = 0;
+	let endNum = 0;
+
+	if (firstNum < lastNum) {
+		startNum = firstNum;
+		endNum = lastNum;
+	} else {
+		startNum = lastNum;
+		endNum = firstNum;
+	}
+
+	for (let i = startNum; i <= endNum; i++) {
 		sum += i;
 	}
 	return sum;
 };
-
-console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
