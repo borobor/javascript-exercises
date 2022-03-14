de63822bd7b97f8ae68c2046bb977db83bfbb852
borobor <bornajurisa@gmail.com>
2022-03-14 22:39:49 +0100

    Create a function that calculates factorials

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -26,8 +26,13 @@ const power = function(a, b) {
 	return a;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+	if (num == 0) return 1;
+	for (let i = 1; i <= num; i++) {
+		result *= i;
+	}
+	return result;
 };
 
 // Do not edit below this line
