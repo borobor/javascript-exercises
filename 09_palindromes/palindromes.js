0d9bc289ba50ae968c8397ffaa1da99c87f3e209
borobor <bornajurisa@gmail.com>
2022-03-17 18:36:06 +0100

    concise function for a single word

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,8 +1,6 @@
 const palindromes = function (palindrome) {
-	let arr = Array.from(palindrome);
-	let reversedArr = arr.reverse();
-	let reversed = reversedArr.join('');
-	return palindrome == reversed;
+	let reversed = Array.from(palindrome).reverse().join('');
+	return palindrome === reversed;
 };
 
 // Do not edit below this line
