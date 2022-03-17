d018c6749178239e04d3348a32fb9641dcf9cc2f
borobor <bornajurisa@gmail.com>
2022-03-17 19:30:34 +0100

    Add alternative .filter() method using regular expressions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,7 +2,8 @@ const palindromes = function (palindrome) {
 	let cleanArr = palindrome
 						.toLowerCase()
 						.split('')
-						.filter(char => char >= 'a' && char <= 'z');
+						// .filter(char => char >= 'a' && char <= 'z');
+						.filter(char => /[a-z]/.test(char));
 	let cleanString = cleanArr.join('');
 	let reversedString = cleanArr.reverse().join('');
 	
