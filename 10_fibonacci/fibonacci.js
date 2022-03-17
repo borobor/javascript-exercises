3521130e1c3f411e72bd5d25b1d7932ced456f3e
borobor <bornajurisa@gmail.com>
2022-03-17 21:28:41 +0100

    Optimise the code

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,16 +1,13 @@
 const fibonacci = function(num) {
-	num = +num;
-	let sequence = [1, 1];
+	let sequence = [0, 1];
 	if (num < 0) return "OOPS";
-	if (num <= 1) return 1;
-	if (num > 1) { 
-		for (let i = 2; i < num; i++) {
-			let last = sequence.slice(-1);
-			let secondLast = sequence.slice(-2, -1);
-			let newFib = +last + +secondLast;
-			sequence.push(newFib);
+	for (let i = 1; i < num; i++) {
+		let last = sequence.slice(-1);
+		let secondLast = sequence.slice(-2, -1);
+		let newFib = +last + +secondLast;
+		sequence.push(newFib);
 		}
-	}	return +sequence.slice(-1);
+	return +sequence.slice(-1);
 };
 
 // Do not edit below this line
