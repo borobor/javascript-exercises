b56eab1e7edc583ed40f6cf8aea65ba429039f44
borobor <bornajurisa@gmail.com>
2022-03-17 20:54:18 +0100

    Finished Fibonacci function using arrays and slice

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+	num = +num;
+	let sequence = [1, 1];
+	if (num < 0) return "OOPS";
+	if (num <= 1) return 1;
+	if (num > 1) { 
+		for (let i = 2; i < num; i++) {
+			let last = sequence.slice(-1);
+			let secondLast = sequence.slice(-2, -1);
+			let newFib = +last + +secondLast;
+			sequence.push(newFib);
+		}
+	}	return +sequence.slice(-1);
 };
 
 // Do not edit below this line
