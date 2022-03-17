905fd1a6ffd432c7e023dc3a8529b55ad7045564
borobor <bornajurisa@gmail.com>
2022-03-17 18:26:06 +0100

    Write a function that works with single words

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+	let arr = Array.from(palindrome);
+	let reversedArr = arr.reverse();
+	let reversed = reversedArr.join('');
+	return palindrome == reversed;
 };
 
 // Do not edit below this line
