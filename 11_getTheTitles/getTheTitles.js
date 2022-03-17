4b4e1b7e13da2416c63e1145aee7c5544e72b5c4
borobor <bornajurisa@gmail.com>
2022-03-17 22:09:01 +0100

    Write the function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,20 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
 
+const getTheTitles = function(arr) {
+	let titles = [];
+	arr.forEach(book => {
+		titles.push(book.title);		
+	});
+	return titles;
 };
 
 // Do not edit below this line
