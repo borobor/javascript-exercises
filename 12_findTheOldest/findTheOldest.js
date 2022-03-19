7c6f49afccb602db4432fe909ae75f3dc1244fc6
borobor <bornajurisa@gmail.com>
2022-03-19 17:35:35 +0100

    Write a function that finds the oldest person using .reduce()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+	return arr.reduce(function (oldest, currentPerson) {
+		const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+		const currentPersonAge = getAge(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
+		return (oldestAge > currentPersonAge) ? oldest : currentPerson;
+	})
 };
+
+const getAge = function(death, birth) {
+	return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
