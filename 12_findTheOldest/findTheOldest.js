5a9727e15b05ece9903b3c387512ee85110149eb
borobor <bornajurisa@gmail.com>
2022-03-19 15:05:28 +0100

    Write a function that sorts the people from oldest to youngest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+	const oldest = arr.sort(function(a, b) {
+		const lastPerson = a.yearOfDeath - a.yearOfBirth;
+		const nextPerson = b.yearOfDeath - b.yearOfBirth;
+		return (nextPerson - lastPerson);
+	});
+	return arr[0];
 };
 
 // Do not edit below this line
